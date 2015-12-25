19e302a66a3fae59dfc779ca07cb4895535e4562
Dylan Stark <dylan.stark@gmail.com>
2015-12-25 15:14:43 -0600

    Fixes range selection.

--- markdown_code_quotes.js
+++ markdown_code_quotes.js
@@ -5,10 +5,13 @@ var selRange = getSelectedRange();
 
 if (!sel || sel.length == 0) {
   setSelectedText("`");
+  setSelectedRange(selRange[0]+1,0);
 }
 else if (1 < sel.split(/\n/).length) {
   setSelectedText("```\n"+sel+"```");
+  setSelectedRange(selRange[0]+selRange[1]+7,0);
 }
 else {
   setSelectedText("`"+sel+"`");
+  setSelectedRange(selRange[0]+selRange[1]+2,0);
 }
